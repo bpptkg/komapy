cfb9802822797b24163fb1eed99a6b1cc892ce24
Indra Rudianto <indrarudianto.official@gmail.com>
2019-08-19 16:20:20 +0700

    Setup PyPI packaging

--- README.md
+++ README.md
@@ -7,37 +7,11 @@ Python library for creating customizable BPPTKG Monitoring API chart.
 KomaPy depends on Python 3.5+ and several packages. You can see them in
 `requirements.txt` file.
 
-KomaPy also depends on library developed at internal BPPTKG project:
-
-* bmaclient>=0.1.0
-
 ## Installation
 
-Download the latest version from GitLab repository and unpack the archive:
+Install the latest version from PyPI by typing this command:
 
-    tar -xvf komapy-v0.1.0.tar.gz
-
-Make Python virtual environment and activate the virtual environment:
-
-    virtualenv -p python3 venv
-    source venv/bin/activate
-
-Install dependency packages:
-
-    cd /path/to/komapy/
-    pip install -r requirements.txt
-
-Install `bmaclient` package. You can download the package from GitLab
-repository:
-
-    tar -xvf bmaclient-v0.1.0.tar.gz
-    cd bmaclient-v0.1.0/
-    pip install -r requirements.txt
-    python setup.py install
-
-Install the package:
-
-    python setup.py install
+    pip install -U komapy
 
 ## Quick Start
 
--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 import os
 
-from distutils.core import setup
-from komapy.version import get_version
+from setuptools import setup, find_packages
 
 
 def read(filename):
@@ -13,21 +12,27 @@ def read(filename):
 
 setup(
     name='komapy',
-    version=get_version(),
+    version='0.1.0',
     description='Python library for creating customizable BPPTKG Monitoring API chart',
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     license='MIT',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'cached_property'
+    ],
     author='Indra Rudianto',
     author_email='indrarudianto.official@gmail.com',
     url='https://gitlab.com/bpptkg/komapy',
-    zip_safe=True,
-    packages=['komapy'],
+    zip_safe=False,
+    packages=find_packages(exclude=['docs', 'examples', 'tests']),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Science/Research',
-        'Natural Language :: Indonesian',
+        'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
