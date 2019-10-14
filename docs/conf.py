09f6032bd12173b9a8756547b3891721cf90abc7
Indra Rudianto <indrarudianto.official@gmail.com>
2019-10-14 17:21:04 +0700

    Bumped version to 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'KomaPy'
 copyright = '{}, BPPTKG'.format(datetime.datetime.now().year)
 author = 'Indra Rudianto'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 
