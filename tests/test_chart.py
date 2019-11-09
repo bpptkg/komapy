c9def48ffdfc56e8e99a75f495aabfbab6340924
Indra Rudianto <indrarudianto.official@gmail.com>
2019-11-09 19:24:12 +0700

    Add test chart properties in test_chart.py module

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -19,6 +19,70 @@ class ChartTest(unittest.TestCase):
         self.assertEqual(len(chart.axes), 0)
         self.assertEqual(len(chart.rendered_axes), 0)
 
+    def test_chart_title(self):
+        chart = Chart({
+            'title': 'Title'
+        })
+        chart.render()
+        self.assertEqual(chart.title, 'Title')
+
+    def test_chart_theme(self):
+        chart = Chart({
+            'theme': 'seaborn'
+        })
+        chart.render()
+        self.assertEqual(chart.theme, 'seaborn')
+
+    def test_chart_timezone(self):
+        chart = Chart({
+            'timezone': 'Asia/Jakarta'
+        })
+        chart.render()
+        self.assertEqual(chart.timezone, 'Asia/Jakarta')
+
+    def test_chart_figure_options(self):
+        chart = Chart({
+            'figure_options': {
+                'num': 2,
+                'figsize': [6, 8],
+                'dpi': '96'
+            }
+        })
+        chart.render()
+        self.assertDictEqual(chart.figure_options, {
+            'num': 2,
+            'figsize': [6, 8],
+            'dpi': '96'
+        })
+
+    def test_chart_save_options(self):
+        chart = Chart({
+            'save_options': {
+                'dpi': 300,
+                'orientation': 'landscape'
+            }
+        })
+        chart.render()
+        self.assertDictEqual(chart.save_options, {
+            'dpi': 300,
+            'orientation': 'landscape'
+        })
+
+    def test_chart_tight_layout(self):
+        chart = Chart({
+            'tight_layout': {
+                'pad': 3,
+                'w_pad': 1,
+                'h_pad': 1
+            }
+        })
+        chart.render()
+        self.assertEqual(chart.tight_layout, {
+            'pad': 3,
+            'w_pad': 1,
+            'h_pad': 1
+        })
+
 
 if __name__ == '__main__':
     unittest.main()
