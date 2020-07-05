0627572f30ac34a213bdb5f006e56b1bf326c7f1
Indra Rudianto <indrarudianto.official@gmail.com>
2020-07-05 16:23:12 +0700

    Update quickstart example

--- komapy/chart.py
+++ komapy/chart.py
@@ -17,20 +17,18 @@ Example:
         'layout': {
             'data': [
                 {
-                    'series': [
-                        {
-                            'name': 'edm',
-                            'query_params': {
-                                'benchmark': 'BAB0',
-                                'reflector': 'RB2',
-                                'start_at': '2019-04-01',
-                                'end_at': '2019-08-01',
-                                'ci': True
-                            },
-                            'fields': ['timestamp', 'slope_distance'],
-                            'xaxis_date': True
-                        }
-                    ]
+                    'series': {
+                        'name': 'edm',
+                        'query_params': {
+                            'benchmark': 'BAB0',
+                            'reflector': 'RB2',
+                            'start_at': '2019-04-01',
+                            'end_at': '2019-08-01',
+                            'ci': True
+                        },
+                        'fields': ['timestamp', 'slope_distance'],
+                        'xaxis_date': True
+                    }
                 }
             ]
         }
